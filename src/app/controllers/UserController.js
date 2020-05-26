cedf3358fe417f97843533432b560fdd399c6026
Luciano de Cezare <lucianodecezare@gmail.com>
2020-05-25 23:05:17 -0300

    Added Update in Users

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -13,6 +13,30 @@ class UserController {
     // Return just the essential
     return res.json({ id, email, name, provider });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    // Verify the email is not in use
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Email already in use' });
+      }
+    }
+
+    // Compare old and new passwords
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, email, name, provider });
+  }
 }
 
 export default new UserController();
