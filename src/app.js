3d4ed701032eaf3480367f5a0bbd49cd1580467a
Luciano de Cezare <lucianodecezare@gmail.com>
2020-07-06 11:47:25 -0300

    Added Sentry and Youch to Handle Errors
    - Added @sentry/node package
    - Added express-async-errors package
    - Added youch package
    - Updated .gitignore

--- src/app.js
+++ src/app.js
@@ -1,23 +1,30 @@
 import express from 'express';
 import { resolve } from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
 
-import { Badges } from './utils';
-
-import { routes } from './routes';
 import './database';
+import { Badges } from './utils';
+import { routes } from './routes';
+import sentryConfig from './config/sentry';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   // Register middlewares
   middlewares() {
     console.log(`\n${Badges.INFO}Starting middlewares`);
 
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -29,6 +36,16 @@ class App {
   routes() {
     console.log(`${Badges.INFO}Starting routes`);
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  // Add exception middleware
+  exceptionHandler() {
+    this.server.use(async (error, req, res, next) => {
+      const errors = await new Youch(error, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry-example.js
@@ -0,0 +1,4 @@
+// Rename this file to sentry.js and put your credentials here
+export default {
+  dsn: ''
+};
